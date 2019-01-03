cfc0e29cf81f00739fcc42a2b187f5d81cd8fd97
CREMINON Celien <celien.creminon@soprasteria.com>
2019-01-03 10:31:31 +0100

    First commit tower of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero' ;
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Hanzo' },
+    { id: 12, name: 'D.Va' },
+    { id: 13, name: 'Junkrat' },
+    { id: 14, name: 'Ana' },
+    { id: 15, name: 'Mei' },
+    { id: 16, name: 'Zarya' },
+    { id: 17, name: 'Ashe' },
+    { id: 18, name: 'Soldier 76' },
+    { id: 19, name: 'Roadhog' },
+    { id: 20, name: 'Zenyatta' },
+];
